b3fd4f222c760971007e934de4946621b8850bf3
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-07-14 14:40:46 +0000

    14th july

---
+++ basics/t5.js
@@ -0,0 +1,19 @@
+const name= "yash"
+const repoCount= 50
+
+//console.log(name+ repoCount + " Value")
+console.log('Hello my name is ${name}')
+
+const gameName = new String('yash')
+
+console.log(gameName[0])
+
+
+
+
+
+
+
+
+
+
