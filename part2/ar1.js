196bb1ba917e51f8f1f6fe1f01b2c9028e874f31
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-07-16 13:45:10 +0000

    3rd

---
+++ part2/ar1.js
@@ -0,0 +1,25 @@
+const myArr = [2,3,4,5,6]
+// const myArr2 = [21,31,41,51,61]
+// // myArr.push(6)
+// // myArr.pop(6)
+
+// // myArr.unshift(0)
+// // myArr.shift(5)
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+
+// console.log(myArr)
+
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// SLICE, splice
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+console.log("B ",myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C ",myArr)
