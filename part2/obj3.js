37c7ec58aacbc864fd452aa939fdb81ba9548654
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-07-23 09:27:53 +0000

    231st

---
+++ part2/obj3.js
@@ -0,0 +1,17 @@
+const course ={
+    coursename:"Js-hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const{courseInstructor: instructor} = course
+//  console.log(instructor);
+
+// {
+//     "name": "Yash",
+//     "coursename": "Js",
+//     "price": "free"
+// }
+
